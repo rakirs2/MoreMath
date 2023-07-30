4fccaa8dbf0863bca4caaab99705a85e5401915b
rakirs2 <srmylavarapu@microsoft.com>
2023-07-30 08:48:51 -0500

    initial commit

---
+++ NumberTheoryTests/NumberTheoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumberTheoryTests;
+
+[TestClass]
+public class NumberTheoryTests
+{
+    [TestMethod]
+    public void IsPrimeTest()
+    {
+        //TODO No DoubleNesting
+        Assert.IsTrue(NumberTheory.NumberTheory.IsPrime(2));
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# MoreMath
+This is a collection of public math utils I've built over time.
+
+Ideally, I'd like this to have some parity with common math functionality in Python but for now this is just a fun side project.
