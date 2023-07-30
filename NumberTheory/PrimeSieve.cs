0ade09e5b9399480ab5664fd153ac339608d1819
rakirs2 <srmylavarapu@microsoft.com>
2023-07-30 15:18:22 -0500

    adding prime sieve

---
+++ NumberTheory/PrimeSieve.cs
@@ -0,0 +1,57 @@
+﻿namespace NumberTheory;
+
+public class PrimeSieve
+{
+    private int _end = 3;
+    
+    public HashSet<int> _primes;
+
+        public List<int> _primesList;
+
+            public PrimeSieve()
+            {
+                _primes = new HashSet<int>() { 2, 3 };
+                _primesList = new List<int>() { 2, 3 };
+    }
+
+private void ExtendRange(int num)
+    {
+        while (_end < num)
+        {
+            _end += 2;
+            if (CalculatePrime(_end))
+            {
+                _primesList.Add(_end);
+                _primes.Add(_end);
+            }
+        }
+        
+    }
+
+    public int MaxPrime()
+    {
+        return _primesList[^1];
+    }
+
+    private bool CalculatePrime(int num)
+    {
+        foreach (var prime in _primesList)
+        {
+            if (_end % prime == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsPrime(int num)
+    {
+        if (_end < num)
+        {
+            ExtendRange(num);
+        }
+        return _primes.Contains(num); ;
+    }
+}
---
+++ NumberTheory/Primes.cs
@@ -0,0 +1,28 @@
+﻿namespace NumberTheory;
+
+public class NumberTheory
+{
+    public static bool IsPrime(int value)
+    {
+        if (value <= 1)
+        {
+            return false;
+        }
+
+        if (value == 2)
+        {
+            return true;
+
+        }
+        if (value % 2==0)
+        {
+            return false;
+
+        }
+        
+        for (var i = 3; i < value; i+=2)
+            if (value % i == 0)
+                return false;
+        return true;
+    }
+}
