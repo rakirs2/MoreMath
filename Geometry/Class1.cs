8f7187baa8cce467415a342a88350c80036519fe
rakirs2 <srmylavarapu@microsoft.com>
2023-07-31 07:49:50 -0500

    adding some scaffolding

---
+++ Geometry/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace Geometry
+{
+    public class Point
+    {
+        
+    }
+    
+}
---
+++ Polynomial/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Polynomial
+{
+    public class Line<T> where T: IComparable<T>, IConvertible, IEquatable<T>
+    {
+        private decimal slope;
+        private decimal intercept;
+
+    }
+}
